6d73bf9bca4d5c3b5136800f4f818ef11213aade
green-quartz <114497500+green-quartz@users.noreply.github.com>
2024-02-28 15:47:46 +0200

    seminar2

---
+++ seminar2/src/main/Program.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import model.clase.Angajat;
+import model.clase.Aplicant;
+import model.readere.AngajatReader;
+import model.readere.AplicantReader;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Aplicant> listaAngajati;
+		AplicantReader angajatReader = new AngajatReader();
+		try {
+			listaAngajati = angajatReader.readAplicanti("angajati.txt");
+			for(Aplicant angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
